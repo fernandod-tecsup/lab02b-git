068a45523a0e09fb8174b953db13b32bf3e0710f
Alumno <Alumno@LAB1504-02>
2020-03-10 17:52:32 -0500

    metodo 3

--- src/main/java/com/tecsup/cps/lab02b_git/App.java
+++ src/main/java/com/tecsup/cps/lab02b_git/App.java
@@ -4,10 +4,17 @@ package com.tecsup.cps.lab02b_git;
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( " Comentario final " );
     }
+    
+    public static void metodo_dev03() {
+    	System.out.println("gaaa");
+    	
+    }
 }
+
