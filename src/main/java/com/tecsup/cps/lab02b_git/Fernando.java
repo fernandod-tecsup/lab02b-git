334bdad1a6702be1de6c74167df3bc7bcdf53402
Fernando Diaz <fernando.diaz@bizlinks.la>
2020-03-11 15:01:03 -0500

    fernandoclass

---
+++ src/main/java/com/tecsup/cps/lab02b_git/Fernando.java
@@ -0,0 +1,16 @@
+package com.tecsup.cps.lab02b_git;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class ClaseFernando 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+    
+}
+
